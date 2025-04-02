2ff30ae6c01236e9b86fd8766eea80692b9999d3
gabu211 <gabriel_deconto@hotmail.com>
2025-04-02 17:15:21 -0300

    first commit

---
+++ mentorados/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+ path('', views.mentorados, name='mentorados'),
+ path('reunioes/', views.reunioes, name='reunioes'),
+ path('auth/', views.auth, name="auth_mentorado"),
+
+]
---
+++ mentorados/views.py
@@ -0,0 +1,93 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, request
+from .models import Mentorados, Navigators, User, DisponibilidadeHorarios
+from django.contrib import messages
+from django.contrib.messages import constants
+from datetime import datetime, timedelta
+
+def mentorados(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    
+    if request.method == 'GET':
+        navigators = Navigators.objects.filter(user=request.user)
+        mentorados = Mentorados.objects.filter(user=request.user)
+
+        estagio_choices = Mentorados._meta.get_field('estagio').choices  
+        estagios_flat = [i[1] for i in estagio_choices]  
+        qtd_estagios = [Mentorados.objects.filter(estagio=i[0], user=request.user).count() for i in estagio_choices]
+
+        return render(
+            request, 
+            'mentorados.html', 
+            {
+                'estagios': estagio_choices,
+                'navigators': navigators,
+                'mentorados': mentorados,
+                'estagios_flat': estagios_flat,
+                'qtd_estagios': qtd_estagios
+            }
+        )
+
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        foto = request.FILES.get('foto')
+        estagio = request.POST.get("estagio")
+        navigator = request.POST.get('navigator')
+
+       
+        estagio_valido = [i[0] for i in Mentorados._meta.get_field('estagio').choices]
+        if estagio not in estagio_valido:
+            estagio = 'E1'  
+
+        
+        try:
+            navigator = int(navigator)
+        except (ValueError, TypeError):
+            navigator = None
+
+        mentorado = Mentorados(
+            nome=nome,
+            foto=foto,
+            estagio=estagio,
+            navigator_id=navigator if navigator else None,
+            user=request.user
+        )
+        
+        mentorado.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Mentorado cadastrado com sucesso!')
+        return redirect('mentorados')
+
+def reunioes(request):
+    if request.method == 'GET':
+        return render(request, 'reunioes.html')
+    elif request.method == 'POST':
+        data = request.POST.get('data')
+        data = datetime.strptime(data, '%Y-%m-%dT%H:%M')
+
+        disponibilidades = DisponibilidadeHorarios.objects.filter(mentor=request.user).filter(
+            data_inicial__gte=(data - timedelta(minutes=50)),
+            data_inicial__lte=(data + timedelta(minutes=50))
+        )
+
+        if disponibilidades.exists():
+            messages.add_message(request, constants.ERROR, 'Você já possui uma reunião em aberto')
+            return redirect('reunioes')
+
+        disponibilidades = DisponibilidadeHorarios(
+            data_inicial=data,
+            mentor=request.user,   
+        )
+        disponibilidades.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Horário disponibilizado com sucesso.')
+        return redirect('reunioes')
+    
+def auth(request):
+ if request.method == 'GET':
+  return render(request, 'auth_mentorado.html')
+ elif request.method == 'POST':
+     token= request.POST.get('token')
+
+     mentorado = Mentorados.objects.filter(token=token)
